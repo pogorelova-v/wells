483f8e6e3e12dcebea7034eaa9c55c9473ec162f
Pogorelova-v <gorelikovavn@gmail.com>
2022-11-17 08:19:01 +0300

    swipe slide job

--- main/script.js
+++ main/script.js
@@ -8,6 +8,14 @@ let slideWidth = document.querySelector('.wrapper-slider').offsetWidth;
 console.log(slideWidth);
 
 slideNext.addEventListener('click', () => {
+    rollSlideNext()
+})
+slidePrev.addEventListener('click', () => {
+    rollSlidePrev()
+})
+
+
+function rollSlideNext(){
     count++;
     slidersLine.style.transition = 'all 0.5s';
     if(count >= slidersArr.length){
@@ -15,9 +23,8 @@ slideNext.addEventListener('click', () => {
         slidersLine.style.transition = 'all 0s';
     }
     rollSlide()
-})
-
-slidePrev.addEventListener('click', () => {
+}
+function rollSlidePrev(){
     count--;
     slidersLine.style.transition = 'all 0.5s';
     if(count < 0){
@@ -25,8 +32,46 @@ slidePrev.addEventListener('click', () => {
         slidersLine.style.transition = 'all 0s';
     }
     rollSlide()
-})
+}
 
 function rollSlide() {
     slidersLine.style.transform = 'translate(-'+count*slideWidth+'px)';
 }
+
+//------------------------swipe-slide
+
+let wrapperSlider = document.querySelector('.wrapper-slider')
+
+wrapperSlider.addEventListener('touchstart', handleTouchStart, false);
+wrapperSlider.addEventListener('touchmove', handleTouchMove, false);
+
+let x1 = null;
+let y1 = null;
+
+function handleTouchStart (event){
+    const firstTouch = event.touches[0];
+    x1 = firstTouch.clientX;
+    y1 = firstTouch.clientY;
+}
+
+function handleTouchMove (event){
+    if(!x1 || !y1){
+        return false;
+    }
+    const lastTouch = event.touches[0];
+    x2 = lastTouch.clientX;
+    y2 = lastTouch.clientY;
+
+    let xDiff = x2 - x1;
+    let yDiff = y2 - y1;
+    if(Math.abs(xDiff) > Math.abs(yDiff)){
+        if(xDiff > 0){
+            rollSlidePrev()
+        } else {
+            rollSlideNext()
+        }
+    }
+    x1 = null;
+    y1 = null;
+}
+ 
